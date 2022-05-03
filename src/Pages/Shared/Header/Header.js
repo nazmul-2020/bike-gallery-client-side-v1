8017e1e2fbfa79f7af9a8942da97b158290525c9
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-04 01:39:19 +0600

    start bike gallery adde navber

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Bike Gallery</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="home">Home</Nav.Link>
+                            <Nav.Link href="#priEcing">Pricing</Nav.Link>
+                            <Nav.Link href="#priGcing">PrJJicing</Nav.Link>
+                            <Nav.Link href="#priHcing">PriFcing</Nav.Link>
+                            <Nav.Link href="#pricWing">PricUing</Nav.Link>
+                            
+                        </Nav>
+                        <Nav>
+                            {/* <Nav.Link href="#deets">More deets</Nav.Link> */}
+                            <Nav.Link eventKey={2} href="#memes">
+                               Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
